cc49e08cd5a63eef4181469f964bdcb75b61e5e7
mahier8 <mahier.sydow8@gmail.com>
2022-01-17 02:21:01 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const mongoose = require("mongoose")
+
+// mongo db connection
+mongoose
+// 1. need to figure out how to connect/create mongodb to connect to  
+// mongoose will create the database for me
+    .connect("mongodb://localhost:27017/", { useNewUrlParser: true })
+    .then(() => {
+        const app = express();
+        
+        app.listen(5000, () => {
+            console.log("Server has started");
+        });
+    });
